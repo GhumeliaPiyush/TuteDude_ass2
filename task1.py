cee450bfb1d53f957196d5b6d6bb85e97b91b8f4
Piyush Ghumelia <piyush.ghumelia@gmail.com>
2025-04-03 11:23:20 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,6 @@
+var = int(input("Enter the Number: "))
+
+if var % 2==0:
+    print(f"{var} is even number")
+else:
+    print(f"{var} is odd number")
---
+++ task2.py
@@ -0,0 +1,5 @@
+sumall = 0
+for i in range(1,51):
+    sumall = sumall+i
+    
+print("The sum of 1 to 50 is: ",sumall)
